8665dcc3fa87404e7c0830f96885c7924d8e0d4f
Fathul <61180722+basyair7@users.noreply.github.com>
2021-11-26 23:20:29 +0700

    Update program_arduino.ino

--- example/soil-program/program_arduino/program_arduino.ino
+++ example/soil-program/program_arduino/program_arduino.ino
@@ -1,43 +1,34 @@
 #include <ArduinoJson.h>
 
-// definisi pin
+// definisi pin sensor
 #define soil_sensor A0
 #define relay 4
 
-void setup(){
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.setTimeout(1);
-  // deklarasi dari pin relay
   relay_pin(relay);
-  
-  // konfigurasi pin sensor
   pinMode(soil_sensor, INPUT);
-  
+
 }
 
-void loop(){
+void loop() {
   // put your main code here, to run repeatedly:
-  // Membuat objek jsonBuffer
+  // Program mengambil data sensor
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& data = jsonBuffer.createObject();
   
-  // Membuat & mengisi nilai variabel data soil sensor
-  int data_soil = map(analogRead(soil_sensor), 0, 1023, 0, 100);
-  
-  // Masukkan data ke dalam array 
-  data["kelembaban"] = data_soil;
-  
-  // Mengirim data ke Serial
+  int nilaiSoil = map(analogRead(soil_sensor), 0, 1023, 0, 100);
+
+  data["kelembaban"] = nilaiSoil;
+
   data.printTo(Serial);
-  // Bersihkan json jika data telah terkirim
   jsonBuffer.clear();
   Serial.println();
-  
-  // Memanggil program relay
-  relay_bord(relay);
-  //relay_proteus(relay);
-  
+
+  relay_2(relay);
+
   delayMicroseconds(500);
+
 }
-  
